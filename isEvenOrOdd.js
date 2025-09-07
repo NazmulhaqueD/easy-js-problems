b1da5a88d75f5d1c052d752c0760068b99dd7978
NazmulhaqueD <mdnajmulhaque982@gmail.com>
2025-09-07 23:20:50 +0600

    Add function to check if a number is even or odd

---
+++ isEvenOrOdd.js
@@ -0,0 +1,13 @@
+// problem: 1
+// Add function to check if a number is even or odd
+
+function isEvenOrOdd(num) {
+    if(num%2==0){
+        return("Even");
+    }
+    else{
+        return("Odd");
+    }
+}
+
+console.log(isEvenOrOdd(6));
