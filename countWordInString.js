fe91039cb1f79db97349bd88509ea5e31ea20a3f
NazmulhaqueD <mdnajmulhaque982@gmail.com>
2025-09-08 11:52:00 +0600

    Add function to count occurrences of a word in a string

---
+++ countWordInString.js
@@ -0,0 +1,15 @@
+// problem: 11
+//count occurrences of a word in a string
+
+function wordCount(sentence, checkWord){
+    const sentenceArr = sentence.split(" ")
+    let count = 0;
+    for(const word of sentenceArr){
+        if(word===checkWord){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(wordCount("hello world hello programming hello", "hello"));
