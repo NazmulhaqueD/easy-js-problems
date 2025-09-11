6bad784dd721d08665e3b88ffde2b3439aaa6af2
NazmulhaqueD <mdnajmulhaque982@gmail.com>
2025-09-11 12:54:28 +0600

    Add function to merge two arrays

---
+++ mergeTwoArrays.js
@@ -0,0 +1,9 @@
+// Problem: 15
+// merge two arrays
+
+function mergeTowArray(arr1, arr2) {
+    const mergeArray = [...arr1, ...arr2];
+    return mergeArray;
+}
+console.log(mergeTowArray([1, 2, 3, 4], [8, 9, 5]));
+;
